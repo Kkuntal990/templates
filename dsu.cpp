f785cdf8ba1100c2e4e88b1bc06aa7970d6eecb5
Kuntal Kokate <kokatekuntal@gmail.com>
2020-04-14 16:03:09 +0530

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,27 @@
+int Find(int x) {
+	int tx, rx;
+	tx = x;
+	while (parent[tx] > 0)
+		tx = parent[tx];
+  // for path compression
+	while (x != tx) {
+		rx = parent[x];
+		parent[x] = tx;
+		x = rx;
+	}
+	
+	return tx;
+}
+
+void Union(int i, int j) {
+	int ti = Find(i), tj = Find(j);
+	if (ti == tj)
+		return;
+	if (size[ti] >= size[tj]) {
+		parent[tj] = ti;
+		size[ti] += size[tj];
+	} else {
+		parent[ti] = tj;
+		size[tj] += size[ti];
+	}
+}
